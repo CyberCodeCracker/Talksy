2a32d9fa647c383c84ac5b11f77e056fe3002bfb
CyberCodeCracker <souhailamouri123@gmail.com>
2025-09-17 16:29:20 +0100

    Initial file and infrastructure configuration

---
+++ Talksy-API/src/main/java/com/amouri_dev/talksy/TalksyApiApplication.java
@@ -0,0 +1,13 @@
+package com.amouri_dev.talksy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalksyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalksyApiApplication.class, args);
+	}
+
+}
---
+++ Talksy-API/src/test/java/com/amouri_dev/talksy/TalksyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amouri_dev.talksy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalksyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
