a6b7458ec934133320cd5d700927b1a1f77d4096
CyberCodeCracker <souhailamouri123@gmail.com>
2025-10-04 11:17:31 +0100

    Implemented the File service

--- Talksy-API/src/main/java/com/amouri_dev/talksy/core/services/FileService.java
+++ Talksy-API/src/main/java/com/amouri_dev/talksy/core/services/FileService.java
@@ -2,16 +2,70 @@ package com.amouri_dev.talksy.core.services;
 
 import com.amouri_dev.talksy.core.Iservices.IFileService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class FileService implements IFileService {
 
+    @Value("${app.file.uploads.media-output-path}")
+    private String fileUploadPath;
 
     @Override
-    public String  saveFile(MultipartFile file, Long senderId) {
+    public String  saveFile(
+            @NonNull MultipartFile sourceFile,
+            @NonNull Long userId
+    ) {
+        // Automatically determines separator from the OS
+        final String fileUploadSubPath = "users" + File.separator + userId;
+        return uploadFile(sourceFile, fileUploadSubPath);
+    }
+
+    private String uploadFile(
+            @NonNull MultipartFile sourceFile,
+            @NonNull String fileUploadSubPath
+    ) {
+        final String finalUploadPath = fileUploadPath + File.separator + fileUploadSubPath ;
+        File targetFolder = new File(finalUploadPath);
+        if (!targetFolder.exists()) {
+            boolean folderCreated = targetFolder.mkdirs();
+            if (!folderCreated) {
+                log.warn("Failed to create target folder, {}", targetFolder);
+                return null;
+            }
+        }
+        final String fileExtension = getFileExtension(sourceFile.getOriginalFilename());
+        String targetFilePath = finalUploadPath + File.separator + System.currentTimeMillis() + fileExtension;
+        Path targetPath = Paths.get(targetFilePath);
+        try {
+            Files.write(targetPath, sourceFile.getBytes());
+            log.info("File saved to {}", targetPath);
+            return targetFilePath;
+        } catch (IOException e) {
+            log.error("File was not saved", e);
+        }
         return null;
+    }
+
+    private String getFileExtension(String fileName) {
+        if (fileName == null || fileName.isEmpty()) {
+            return "";
+        }
+        final int lastDotIndex = fileName.lastIndexOf(".");
+        if (lastDotIndex == -1) {
+            return "";
+        }
+        return fileName.substring(lastDotIndex + 1).toLowerCase();
     }
 }
