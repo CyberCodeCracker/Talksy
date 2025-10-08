f006dc1d6a3c83390305abcef51e19058bb080c8
CyberCodeCracker <souhailamouri123@gmail.com>
2025-10-08 21:11:38 +0100

    Added missing services not previously generated by openapi

--- talksy-ui/src/app/pages/login/login.component.ts
+++ talksy-ui/src/app/pages/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, inject } from '@angular/core';
 import { AuthenticationRequest } from '../../services/models';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
+import { register } from '../../services/functions';
+import { AuthenticationService } from '../../services/services';
 
 @Component({
   selector: 'app-login',
@@ -19,13 +21,31 @@ export class LoginComponent {
     password: new FormControl(''),
   })
   router = inject(Router);
+  authService = inject(AuthenticationService);
 
   addError(error: string) {
     this.errorMsgs.push(error);  
   }
 
-  onSubmit() {
-
+  login() {
+    this.errorMsgs = [];
+    this.authRequest.email = this.loginForm.value.email ?? '';
+    this.authRequest.password = this.loginForm.value.password ?? '';
+    this.authService.register({
+      body: this.authRequest
+    }).subscribe({
+      next: (response) => {
+        localStorage.setItem('authToken', response.token ?? '');
+        this.router.navigate(['/home']);
+      },
+      error: (err) => {
+        if (err.status === 401) {
+          this.addError('Invalid email or password.');
+        } else {
+          this.addError('An error occurred. Please try again later.');
+        }
+      }
+    });
   }
 
   navigateToRegister() {
